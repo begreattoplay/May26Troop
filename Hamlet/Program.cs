0144b5a311e2c4550f78a663722f8472d48f61e4
julior@gmail.com <julior@gmail.com>
2015-06-01 10:19:06 -0500

    Hamlet exercise

---
+++ Hamlet/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Hamlet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hamletFile = Regex.Replace(File.ReadAllText("ham.txt").Replace("\r\n", " "), @"[^\w\s]", "");
+            List<string> ignoreList = new List<string> { "the", "and", "to", "I", "my", "you", "not", "of", "a" };
+
+
+            var result = hamletFile
+                .Split(' ')
+                .Where(t => !string.IsNullOrWhiteSpace(t) &&
+                    !ignoreList.Any(i => t.IndexOf(i, 0, StringComparison.CurrentCultureIgnoreCase) != -1))
+                .GroupBy(t => t, StringComparer.InvariantCultureIgnoreCase)
+                .OrderByDescending(t => t.Count()).FirstOrDefault();
+
+            Console.WriteLine("The most used not boring word was \"{0}\" and it was used {1} times.", result.Key, result.Count());
+            Console.ReadLine();
+        }
+    }
+}
