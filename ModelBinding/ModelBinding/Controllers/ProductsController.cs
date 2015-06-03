2cf96864ab4346f27c1a853831411f24269bc14e
julior@gmail.com <julior@gmail.com>
2015-06-03 11:08:25 -0500

    Model Binding Sample

---
+++ ModelBinding/ModelBinding/Controllers/ProductsController.cs
@@ -0,0 +1,103 @@
+﻿using ModelBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelBinding.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            Product product = new Product();
+
+            return View(product);
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            //Product p = new Product();
+            //foreach (var item in Request.Form)
+            //{
+            //    var i = (KeyValuePair<string, string>)item;
+            //    if (i.Key == "Name")
+            //        p.Name = i.Value;
+
+            //    if (i.Key == "Price")
+            //        p.Price = decimal.Parse(i.Value);
+            //}
+            try
+            {
+                
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
