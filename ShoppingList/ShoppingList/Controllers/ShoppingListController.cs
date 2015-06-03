90c69e054ac5c2b74da3ff03a3618b63db1f3221
julior@gmail.com <julior@gmail.com>
2015-06-03 15:54:10 -0500

    Day 7: ASP.NET MVC Views In class

---
+++ ShoppingList/ShoppingList/Controllers/ShoppingListController.cs
@@ -0,0 +1,25 @@
+﻿using ShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingList.Controllers
+{
+    public class ShoppingListController : Controller
+    {
+        // GET: ShoppingList
+        public ActionResult Index()
+        {
+            ShoppingListService service = new ShoppingListService();
+
+            IndexViewModel vm = new IndexViewModel
+            {
+                Customers = service.GetItems()
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ ShoppingList2/ShoppingList2/Models/ShoppingListService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingList2.Models
+{
+    class ShoppingListService
+    {
+        private IList<Product> _products = new List<Product>();
+
+        public IList<Product> GetItems()
+        {
+            _products.Add(new Product { Name = "Laptop", Price = 300 });
+            _products.Add(new Product { Name = "Book", Price = 20 });
+            return _products;
+        }
+
+    }
+}
