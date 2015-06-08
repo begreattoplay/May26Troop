d3f83bb9370936c83cf52279f6ab10ed81dc5f7e
julior@gmail.com <julior@gmail.com>
2015-06-08 12:59:53 -0500

    Day 9: EF and Associations

---
+++ Day9Associations/Day9Associations/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Day9Associations.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day9Associations.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            MoviesDbContext db = new MoviesDbContext();
+            var movies = db.Movies.ToList();
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
